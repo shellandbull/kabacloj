c3b5470e20f209a59e46c7aaf27911b89662dba5
Mario Gintili <mariojgintili@gmail.com>
2024-08-07 16:25:15 +0100

    come up with categorical->numerical mappings for the dataset, now all I need is to fit the model

--- src/index.clj
+++ src/index.clj
@@ -21,14 +21,13 @@
 (tc/unique-by ds :coffee_name)
 
 (defn coffee-has-milk [str]
-  (some #(= str %) ["Latte" "American With Milk" "Cortado" "Cappuccino"]))
+  (boolean (some #(= str %) ["Latte" "American With Milk" "Cortado" "Cappuccino"])))
 
 (defn coffee-has-chocolate [str]
-  (some #(= str %) ["Cocoa" "Hot Chocolate"]))
+  (boolean (some #(= str %) ["Cocoa" "Hot Chocolate"])))
 
 (def coffee-names
-  (apply list (-> ds
-                  (tc/select-columns [:coffee_name]))))
+  (distinct (apply list (:coffee_name ds))))
 
 coffee-names
 
@@ -58,14 +57,40 @@ categorical-feature-columns)
 
 (tc/column-names with-features)
 (tds/descriptive-stats with-features)
+
 (-> with-features
-(:with-milk)
-frequencies)
+    (:with-milk)
+    frequencies)
 
 
 (def relevant-with-features-ds
-(-> with-features
-    (tc/select-columns (conj categorical-feature-columns target-column))
-    (tds/drop-missing)
-    (tds/categorical->number [target-column] coffee-names :float64)
-    (ds-mod/set-inference-target target-column)))
+  (-> with-features
+      (tc/select-columns (conj categorical-feature-columns target-column))
+      (tds/drop-missing)
+      (tds/categorical->number [target-column] coffee-names :float64)
+      (ds-mod/set-inference-target target-column)))
+
+(def cat-maps
+  [
+   (ds-cat/fit-categorical-map relevant-with-features-ds :coffee_name coffee-names :float64)
+   (ds-cat/fit-categorical-map relevant-with-features-ds :with-milk [true false] :float64)
+   (ds-cat/fit-categorical-map relevant-with-features-ds :with-chocolate [true false] :float64)
+   ])
+
+cat-maps
+
+(def numeric-ds
+  (reduce (fn [ds cat-map]
+            (ds-cat/transform-categorical-map ds cat-map))
+          relevant-with-features-ds
+          cat-maps))
+
+(tc/head numeric-ds 10)
+
+(def split
+  (first
+   (tc/split->seq numeric-ds :holdout {:seed 112723})))
+
+split
+
+;; https://scicloj.github.io/noj/noj_book.ml_basic.html#train-a-model
