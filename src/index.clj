36017ef5be3552f4d75b152713bcfc9ee6066199
Mario Gintili <mariojgintili@gmail.com>
2024-08-13 14:42:19 +0100

    finish the exploration. We conclude that although a model ~90% accurate can be built. There isnt a strong relationship between your coffee having milk or chocolate and the type of payment you use cash or card

--- src/index.clj
+++ src/index.clj
@@ -11,14 +11,20 @@
             [tablecloth.api.split :as split]
             [scicloj.metamorph.ml :as ml]
             [tech.v3.dataset :as tds]
+            [camel-snake-kebab.core :as csk]
             [scicloj.metamorph.ml.loss :as loss]
             [scicloj.ml.smile.classification]))
+
+
+;; #### Fetching data
 
 (def ds
   (-> "data/index.csv"
       (tc/dataset {:key-fn keyword})))
 
 (tc/unique-by ds :coffee_name)
+
+;; #### Feature engineering
 
 (defn coffee-has-milk [str]
   (boolean (some #(= str %) ["Latte" "American With Milk" "Cortado" "Cappuccino"])))
@@ -49,10 +55,10 @@ coffee-names
 (def target-column :cash_type)
 
 (map
-#(hash-map
-  :col-name %
-  :values  (distinct (get with-features %)))
-categorical-feature-columns)
+ #(hash-map
+   :col-name %
+   :values  (distinct (get with-features %)))
+ categorical-feature-columns)
 
 
 (tc/column-names with-features)
@@ -63,11 +69,14 @@ categorical-feature-columns)
     frequencies)
 
 
+;; #### Preparing the features to be fit into a model
+
+
 (def relevant-with-features-ds
   (-> with-features
       (tc/select-columns (conj categorical-feature-columns target-column))
       (tds/drop-missing)
-      (tds/categorical->number [target-column] coffee-names :float64)
+      (tds/categorical->number [target-column] ["card" "cash"] :float64)
       (ds-mod/set-inference-target target-column)))
 
 (def cat-maps
@@ -85,7 +94,10 @@ cat-maps
           relevant-with-features-ds
           cat-maps))
 
-(tc/head numeric-ds 10)
+(tc/head numeric-ds 500)
+
+
+;; #### Mounting the model
 
 (def split
   (first
@@ -103,6 +115,37 @@ model-with-logistic-regression
 (def prediction-with-logistic-regression
   (ml/predict (:test split) model-with-logistic-regression))
 
-(-> prediction-with-dummy-classifier
-    :survived
-    frequencies)
+;; #### Measuring model accuracy
+
+;; Accuracy of a logistic regression
+
+(loss/classification-accuracy
+ (:cash_type (ds-cat/reverse-map-categorical-xforms (:test split)))
+ (:cash_type (ds-cat/reverse-map-categorical-xforms prediction-with-logistic-regression)))
+
+(def rf-model
+  (ml/train (:train split) {:model-type :smile.classification/random-forest}))
+
+(def rf-prediction
+  (ml/predict (:test split) rf-model))
+
+;; Accuracy of a random forest model
+
+(loss/classification-accuracy
+ (:cash_type (ds-cat/reverse-map-categorical-xforms (:test split)))
+ (:cash_type (ds-cat/reverse-map-categorical-xforms rf-prediction)))
+
+(->>
+ (map
+  (fn [predictor importance]
+    (hash-map :predictor (-> predictor str csk/->kebab-case-keyword)
+              :importance importance))
+  (-> rf-model ml/thaw-model .formula .predictors)
+  (-> rf-model ml/thaw-model .importance))
+ (sort-by :importance)
+ reverse)
+
+;; We conclude that although a model with ~90% accuracy was built. Not a single of it's features holds
+;; a relevant relationship to the target.
+
+;;  This means that it's hard to guess if you will pay cash or card if we know what type of coffee you're getting. And wether or not it has milk or chocolate in it
